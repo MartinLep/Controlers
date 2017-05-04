14c66500905e0554d58a7c72e266fb54f84b4670
MartinLee <250021952@qq.com>
2017-05-04 22:23:02 +0800

    no message

---
+++ pwControls/Classes/Controllers/Controller/父子控制器/pwParentViewController.h
@@ -0,0 +1,13 @@
+//
+//  pwParentViewController.h
+//  pwControls
+//
+//  Created by MartinLee on 17/5/4.
+//  Copyright © 2017年 MartinLee. All rights reserved.
+//
+
+#import "pwBaseViewController.h"
+
+@interface pwParentViewController : pwBaseViewController
+
+@end
---
+++ pwControls/Classes/Controllers/Controller/父子控制器/pwParentViewController.m
@@ -0,0 +1,42 @@
+//
+//  pwParentViewController.m
+//  pwControls
+//
+//  Created by MartinLee on 17/5/4.
+//  Copyright © 2017年 MartinLee. All rights reserved.
+//
+
+#import "pwParentViewController.h"
+
+@interface pwParentViewController ()
+@property (weak, nonatomic) IBOutlet UIView *titleViews;
+@property (weak, nonatomic) IBOutlet UIView *containtView;
+
+@end
+
+@implementation pwParentViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self setUpAllViewControllers];
+}
+
+- (void)setUpAllViewControllers{
+    UIViewController *view1 = [[UIViewController alloc] init];
+    view1.title = @"第1个";
+    [self addChildViewController:view1];
+    
+    UIViewController *view2 = [[UIViewController alloc] init];
+    view2.title = @"第2个";
+    [self addChildViewController:view2];
+    
+    UIViewController *view3 = [[UIViewController alloc] init];
+    view3.title = @"第3个";
+    [self addChildViewController:view3];
+}
+
+
+- (IBAction)showViewControllerView:(UIButton *)sender {
+}
+
+@end
